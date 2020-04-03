551b6301f00502e6f40c9b577b1a6060d7326f37
wiby <wibybramantyo123@gmail.com>
2020-04-03 23:46:25 +0700

    revisi ex7

--- exercise7.js
+++ exercise7.js
@@ -1,8 +1,10 @@
 function hitungJumlahKata(kalimat) {
     // you can only write your code here!
     var jumkal= 1
+    var cek = false
     for(i = 0 ; i <kalimat.length-1 ;i++){
       if (kalimat[i,i+1] == ' '){
+        cek = true
         jumkal++
       }
     }
@@ -14,4 +16,4 @@ function hitungJumlahKata(kalimat) {
   console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
   console.log(hitungJumlahKata('A song to sing')); // 4
   console.log(hitungJumlahKata('I')); // 1
-  console.log(hitungJumlahKata('I believe I can code')); // 5
+  console.log(hitungJumlahKata('I believe I can code')); // 5‘ Berapa Jumlah kata ini? ‘
